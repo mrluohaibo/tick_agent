a094767ad95ee35c97bb7261db717449b71358b7
罗海波 <luohb@zhihuihuiwu.com>
2025-12-28 20:07:15 +0800

    ...

---
+++ bz_core/stock_dict.py
@@ -0,0 +1,31 @@
+
+
+stock_dict_zh_2_en =  {
+            "序号":"index",
+            "代码":"stock_code",
+            "名称":"stock_name",
+            "最新价":"new_quota",
+            "涨跌幅":"price_change",
+            "涨跌额":"price_change_amount",
+            "成交量":"trading_volume",
+            "成交额":"transaction_volume",
+            "振幅":"amplitude",
+            "最高":"highest",
+            "最低":"lowest",
+            "今开":"today_open",
+            "昨收":"yesterday_close",
+            "量比":"volume_ratio",
+            "换手率":"turnover_rate",
+            "市盈率-动态":"pe_dynamic",
+            "市净率":"price-to-book_ratio",
+            "总市值":"total_market_capitalization",
+            "流通市值":"market_capitalization",
+            "涨速":"rate_of_increase",
+            "5分钟涨跌":"5-minute_price_fluctuation",
+            "60日涨跌幅":"60-day_price_change",
+            "年初至今涨跌幅":"year-to-date_price_change",
+
+}
+
+stock_dict_en_2_zh = {v: k for k, v in stock_dict_zh_2_en.items()}
+
