01cfea0f9e8e0a769f939fab286a4aa7fe7d949c
罗海波 <luohb@zhihuihuiwu.com>
2026-01-10 21:12:33 +0800

    url to markdown agent completed

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import my_akshare as ak
-import logging as logger
+import utils.logger_config as logger
+
+from bz_core.stock_info_api import StockInfo
 
 if __name__ == '__main__':
-    stock_zh_a_spot_em_df = ak.stock_zh_a_spot_em()
-    print(stock_zh_a_spot_em_df)
-    print("1")
-    logger.info("log")
+    stock_info = StockInfo()
+    # stock_info.update_stock_industry()
+    # stock_info.read_stock_k_daily_data("601166",'1990-12-19',"2026-01-08")
+    # stock_info.query_history_key_data("688125")
+    # stock_info.query_all_stock_history_k_daily_data()
+    stock_info.query_stock_intro('601166')
+    logger.info("-------------ok!!!-------------------------")
